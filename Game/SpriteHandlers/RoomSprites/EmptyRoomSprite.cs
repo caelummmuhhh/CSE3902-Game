22dc48bfada9dc640a381b5784ea13b475c538da
Kenneth Bevan <bevan.41@buckeyemail.osu.edu>
2024-03-01 18:24:02 -0500

    Added CSV files. Added layering to room/door drawing although it doesn't work yet. Combined Room sprites to a single room object.

--- Game/SpriteHandlers/RoomSprites/EmptyRoomSprite.cs
+++ Game/SpriteHandlers/RoomSprites/EmptyRoomSprite.cs
@@ -3,15 +3,15 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace MainGame.SpriteHandlers.ItemSprites
 {
-    public class DoorSegmentNorthSouth : StaticSpriteWithOffset
+    public class EmptyRoomSprite : StaticSpriteWithOffset
     {
         private readonly SpriteBatch spriteBatch;
 
-        public DoorSegmentNorthSouth(
+        public EmptyRoomSprite(
             Texture2D texture,
             SpriteBatch spriteBatch,
-            int spriteHeight = 16,
-            int spriteWidth = 32,
+            int spriteHeight = 0,
+            int spriteWidth = 0,
             int textureStartingX = 0,
             int textureStartingY = 0,
             int scale = 1) : base(texture, spriteHeight, spriteWidth,
@@ -22,12 +22,12 @@ namespace MainGame.SpriteHandlers.ItemSprites
 
         public override void Update() { /* not needed here */ }
 
-        public override void Draw(float x, float y, Color color, float layerDepth)
+        public override void Draw(float x, float y, Color color, float layerDepth = 1.0f)
         {
             Rectangle srcRectangle = GetSourceRectangle();
             Rectangle destRectangle = new Rectangle(
-                (int)x,
-                (int)y,
+                0,
+                0,
                 FrameWidth * Scale,
                 FrameHeight * Scale);
 
