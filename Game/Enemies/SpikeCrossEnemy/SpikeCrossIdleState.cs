990e516356c202271e033b7c220eb27f793db958
caelummmuhhh <caelu2.0@gmail.com>
2024-03-04 10:36:15 -0500

    create initial framework for spike cross enemy
    Create a Spike Cross enemy class that behaves the way it's supposed to, butt here are some things that needs to be ironed out. Like the actual speeds and the max moving distance (which can vary depending on the room, and x and y max moving distance can be different).

---
+++ Game/Enemies/SpikeCrossEnemy/SpikeCrossIdleState.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace MainGame.Enemies
+{
+	public class SpikeCrossIdleState : IEnemyState
+    {
+        private readonly SpikeCrossEnemy entity;
+
+        public SpikeCrossIdleState(SpikeCrossEnemy enemy)
+		{
+            entity = enemy;
+        }
+
+        public void Update()
+        {
+            CardinalDirections direction;
+
+            // FIXME: Spike Cross actually start moving when player is in the same grid block it's in
+            if (Math.Abs(entity.Player.Position.X - entity.Position.X) <= 48)
+            {
+                direction = entity.Player.Position.Y < entity.Position.Y ? CardinalDirections.North : CardinalDirections.South;
+            }
+            else if (Math.Abs(entity.Player.Position.Y - entity.Position.Y) <= 48)
+            {
+                direction = entity.Player.Position.X < entity.Position.X ? CardinalDirections.West : CardinalDirections.East;
+            }
+            else
+            {
+                return;
+            }
+
+            entity.State = new SpikeCrossMovingState(entity, direction);
+        }
+
+        public void Draw() => entity.Draw();
+
+        public void Move() { }
+    }
+}
+
