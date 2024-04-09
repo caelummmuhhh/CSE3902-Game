f8c7ead93d2e6f306402bbaa6cac06e81898bbac
Jacob Kolaczkowski <JakeClashes10@gmail.com>
2024-04-09 14:05:37 -0400

    added some random song i found on the internet,basic framework laid out for the real music and sound effects

--- Game/Game1.cs
+++ Game/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Media;
 
 using MainGame.SpriteHandlers;
 using MainGame.Controllers;
@@ -24,6 +25,7 @@ public class Game1 : Game
     public IPlayer Player;
     public CollisionDetector Collision;
     public GameRoomManager RoomManager;
+    public Song song;
 
     public BlockSprite testBlock; // TODO: DELETE ME
 
@@ -63,6 +65,10 @@ public class Game1 : Game
 
         controllers.Add(new KeyboardController(this, Player));
         controllers.Add(new MouseController(this, Player));
+
+        // temporary fuctionality check
+        song = Content.Load<Song>("Audio/song");
+        MediaPlayer.Play(song);
     }
 
     protected override void Update(GameTime gameTime)
@@ -71,6 +77,7 @@ public class Game1 : Game
         {
             controllers[i].Update();
         }
+        
         RoomManager.Update();
         Player.Update();
         Collision.Update();
