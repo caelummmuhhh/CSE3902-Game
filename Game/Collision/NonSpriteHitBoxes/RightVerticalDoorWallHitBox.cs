b0ddfe2cf9e1bdedda8242119ac4434be556d5b0
caelummmuhhh <caelu2.0@gmail.com>
2024-04-09 01:38:54 -0400

    Merge main into this

--- Game/Collision/NonSpriteHitBoxes/RightVerticalDoorWallHitBox.cs
+++ Game/Collision/NonSpriteHitBoxes/RightVerticalDoorWallHitBox.cs
@@ -17,9 +17,6 @@ namespace MainGame.Collision
                 x: topWall.X,
                 y: topWall.Bottom + Constants.BlockSize,
                 width: 2 * Constants.BlockSize,
-                height: 5 * Constants.BlockSize
-                );
-
             HitBoxes.Add(topWall);
             HitBoxes.Add(bottomWall);
         }
