916b522d1d6b0f92931ce0d64166c190b1e15279
caelummmuhhh <caelu2.0@gmail.com>
2024-04-30 11:14:51 -0400

    suspend game reset command and fix goriya

--- Game/Commands/ResetGameCommand.cs
+++ Game/Commands/ResetGameCommand.cs
@@ -18,21 +18,21 @@ namespace MainGame.Commands
 
         public void Execute()
         {
+            /*
             AudioManager.ResetSong();
 
             game.RoomManager = new(game);
             game.Player = new Player(new Vector2(120 * Constants.UniversalScale, (128 * Constants.UniversalScale) + Constants.HudAndMenuHeight), game.RoomManager,
                 Array.Empty<int>(), game.Dungeon.PlayerStartingHealth, game.Dungeon.PlayerStartingRupees, game.Dungeon.PlayerStartingKeys, game.Dungeon.PlayerStartingBombs);
 
-            game.RoomManager.LoadAllRooms(game.Player);
+            //game.RoomManager.LoadAllRooms(game.Player);
 
             game.Collision = new(game);
 
             game.controllers.Clear();
             game.controllers.Add(new KeyboardController(game, game.Player));
             game.controllers.Add(new MouseController(game, game.Player));
-
-
+            */
         }
 
         public void UnExecute()
